d6c6c618f279f3272022b724259fce0102512c45
septa97 <jancharles.adona@gmail.com>
2021-05-05 01:01:43 +0800

    Add day 7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -12,11 +12,14 @@ fn main() {
             let mut matches = Vec::new();
 
             for mat in bag_re.find_iter(line) {
-                matches.push(mat.as_str());
+                // TODO: I think using match groups is better? research on this more
+                let match_str_vec: Vec<&str> = mat.as_str().split(' ').collect();
+                let desc = format!("{} {}", match_str_vec[0], match_str_vec[1]);
+                matches.push(desc);
             }
 
-            let key = matches[0].to_string();
-            let value = matches[1..].iter().map(|&e| e.to_string()).collect();
+            let key = matches[0].clone(); // TODO: research on the difference of .clone() and .to_owned()
+            let value: Vec<String> = matches[1..].to_vec();
 
             (key, value)
         })
@@ -24,7 +27,7 @@ fn main() {
 
     let mut count = 0;
     for key in bag_map.keys() {
-        if key != "shiny gold bags" && bfs(&bag_map, key) {
+        if key != "shiny gold" && find_recursively(&bag_map, key) {
             count += 1;
         }
     }
@@ -32,30 +35,18 @@ fn main() {
     println!("part 1: {}", count);
 }
 
-// TODO: do I really need BFS/DFS here?
-fn bfs(bag_map: &HashMap<String, Vec<String>>, root: &String) -> bool {
-    let mut deque: VecDeque<String> = VecDeque::new();
-    let mut discovered: HashMap<String, bool> = HashMap::new(); // I could use a HashSet but it would result in an O(log n) find (my assumption, atleast)
-    discovered.insert(root.clone(), true);
-    deque.push_back(root.clone());
-
-    while !deque.is_empty() {
-        if let Some(curr) = deque.pop_front() {
-            if curr == "shiny gold bags" || curr == "shiny gold bag" {
-                // TODO: I could memoize this
-                return true;
-            }
-
-            if let Some(edges) = bag_map.get(&curr) {
-                for edge in edges {
-                    if let None = discovered.get(edge) {
-                        discovered.insert(edge.clone(), true);
-                        deque.push_back(edge.clone());
-                    }
-                }
-            }
-        }
+fn find_recursively(bag_map: &HashMap<String, Vec<String>>, curr: &String) -> bool {
+    if curr == "shiny gold" {
+        return true;
+    } else if curr == "no other" {
+        return false;
     }
 
-    false
+    if let Some(edges) = bag_map.get(curr) {
+        return edges
+            .into_iter()
+            .fold(false, |acc, edge| acc || find_recursively(bag_map, edge));
+    } else {
+        return false;
+    }
 }
