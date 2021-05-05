d3941840faad7b5563a5492ecd4faf0ea0309031
septa97 <jancharles.adona@gmail.com>
2021-05-06 00:12:51 +0800

    Add day 7 part 2 and refactor

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,50 +3,72 @@ use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let bag_re = Regex::new(r"\w+ \w+ bags?").unwrap();
+    let bag_re = Regex::new(r"(\w+ \w+) bags").unwrap();
+    let bag_count_re = Regex::new(r"([1-9] )?(\w+) (\w+) bags?").unwrap();
+    let no_bag_re = Regex::new(r"no other bags").unwrap();
 
-    let bag_map: HashMap<String, Vec<String>> = fs::read_to_string("input/input.txt")
+    let bag_map: HashMap<String, Vec<(String, usize)>> = fs::read_to_string("input/input.txt")
         .expect("Failed to read file!")
         .lines()
+        .filter(|&line| no_bag_re.find(line).is_none())
         .map(|line| {
-            let mut matches = Vec::new();
+            let mut values = Vec::new();
 
-            for mat in bag_re.find_iter(line) {
-                // TODO: I think using match groups is better? research on this more
-                let match_str_vec: Vec<&str> = mat.as_str().split(' ').collect();
-                let desc = format!("{} {}", match_str_vec[0], match_str_vec[1]);
-                matches.push(desc);
+            // .unwrap() cause we're sure that there's a match (or else, there's a bug in the code)
+            let key_match_str = bag_re.find(line).unwrap().as_str();
+            let key = key_match_str[..key_match_str.len() - 5].to_string();
+
+            let mut cap_iter = bag_count_re.captures_iter(line);
+            cap_iter.next(); // to remove the first match, which is the `key` (not sure if there's a cleaner way)
+
+            for cap in cap_iter {
+                let color = format!("{} {}", &cap[2], &cap[3]);
+                let count: usize = cap[1].trim().parse().expect("Failed to parse to usize!"); // not reference for string on stack (&cap[1]) ???
+                values.push((color, count));
             }
 
-            let key = matches[0].clone(); // TODO: research on the difference of .clone() and .to_owned()
-            let value: Vec<String> = matches[1..].to_vec();
-
-            (key, value)
+            (key, values)
         })
         .collect();
 
-    let mut count = 0;
-    for key in bag_map.keys() {
-        if key != "shiny gold" && find_recursively(&bag_map, key) {
-            count += 1;
-        }
-    }
-
-    println!("part 1: {}", count);
+    println!("part 1: {}", part1(&bag_map));
+    println!("part 2: {}", part2(&bag_map));
 }
 
-fn find_recursively(bag_map: &HashMap<String, Vec<String>>, curr: &String) -> bool {
+fn part1(bag_map: &HashMap<String, Vec<(String, usize)>>) -> usize {
+    return bag_map
+        .keys()
+        .filter(|&key| key != "shiny gold" && count_shiny_gold(bag_map, key))
+        .count();
+}
+
+fn count_shiny_gold(bag_map: &HashMap<String, Vec<(String, usize)>>, curr: &String) -> bool {
     if curr == "shiny gold" {
         return true;
-    } else if curr == "no other" {
-        return false;
     }
 
     if let Some(edges) = bag_map.get(curr) {
-        return edges
-            .into_iter()
-            .fold(false, |acc, edge| acc || find_recursively(bag_map, edge));
+        return edges.into_iter().fold(false, |acc, (edge, _)| {
+            acc || count_shiny_gold(bag_map, edge)
+        });
     } else {
         return false;
+    }
+}
+
+fn part2(bag_map: &HashMap<String, Vec<(String, usize)>>) -> usize {
+    return count_inside_shiny_gold(bag_map, &String::from("shiny gold"));
+}
+
+fn count_inside_shiny_gold(
+    bag_map: &HashMap<String, Vec<(String, usize)>>,
+    curr: &String,
+) -> usize {
+    if let Some(value) = bag_map.get(curr) {
+        return value.into_iter().fold(0, |acc, (color, count)| {
+            acc + count + count * count_inside_shiny_gold(bag_map, color)
+        });
+    } else {
+        return 0;
     }
 }
