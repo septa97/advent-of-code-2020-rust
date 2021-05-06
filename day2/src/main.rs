994b2fbd081975b0c38ea4b12f2a31d5135308e7
septa97 <jancharles.adona@gmail.com>
2021-05-06 21:02:25 +0800

    Refactor day 2, use Regex capture groups instead of a lot of .split

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use regex::Regex;
 use std::collections::HashMap;
+use std::fs;
 
 struct Row {
     min: u32,
@@ -9,54 +10,58 @@ struct Row {
 }
 
 fn main() {
+    let line_re = Regex::new(r"(\d{1,2})-(\d{1,2}) ([a-z]): ([a-z]+)").unwrap();
+
     let rows: Vec<Row> = fs::read_to_string("input/input.txt")
         .expect("Failed to read file!")
         .lines()
-        .map(|line | {
-            // TODO: uhhhhh, too much .split here, not sure if there's a better way
-            // maybe deconstructors? (if possible)
-            let main_split: Vec<&str> = line.split(':').collect();
-            let left = main_split[0];
-            let right = main_split[1];
-            let s  = right.trim().to_string();
+        .map(|line| {
+            let cap = line_re.captures(line).unwrap();
+            let min: u32 = cap[1].parse().expect("Failed to parse number to u32");
+            let max: u32 = cap[2].parse().expect("Failed to parse number to u32");
+            let c = cap[3].chars().nth(0).unwrap();
+            let s = cap[4].to_string();
 
-            let left_split: Vec<&str> = left.split(' ').collect();
-            let c = left_split[1].as_bytes()[0] as char; // we're sure that it's ASCII only and that it's only a single character
-            let min_max_split: Vec<&str> = left_split[0].split('-').collect();
-            let min: u32 = min_max_split[0].parse().expect("Failed to parse number to u32");
-            let max: u32 = min_max_split[1].parse().expect("Failed to parse number to u32");
+            Row { min, max, c, s }
+        })
+        .collect();
 
-            Row {min, max, c, s }
-        }).collect();
+    let part1_count = rows
+        .iter()
+        .map(|row| {
+            let mut count_map = HashMap::new();
 
-    let part1_count = rows.iter().map(|row | {
-      let mut count_map = HashMap::new();
+            for ch in row.s.chars() {
+                let count = count_map.entry(ch).or_insert(0u32);
+                *count += 1;
+            }
 
-      for ch in row.s.chars() {
-          let count = count_map.entry(ch).or_insert(0u32);
-          *count += 1;
-      }
+            let count = if let Some(count) = count_map.get(&row.c) {
+                *count
+            } else {
+                0
+            };
 
-      let count = if let Some(count) = count_map.get(&row.c) {
-          *count
-      } else {
-          0
-      };
+            count >= row.min && count <= row.max
+        })
+        .filter(|&e| e)
+        .count();
 
-      count >= row.min && count <= row.max
-    }).filter(|&e| e).count();
+    let part2_count = rows
+        .iter()
+        .map(|row| {
+            let bytes = row.s.as_bytes();
 
-    let part2_count = rows.iter().map(|row| {
-        let bytes = row.s.as_bytes();
+            let left_idx = (row.min - 1) as usize;
+            let right_idx = (row.max - 1) as usize;
+            let left = bytes[left_idx] as char == row.c;
+            let right = bytes[right_idx] as char == row.c;
 
-        let left_idx = (row.min-1) as usize;
-        let right_idx = (row.max-1) as usize;
-        let left = bytes[left_idx] as char == row.c;
-        let right = bytes[right_idx] as char == row.c;
+            left ^ right
+        })
+        .filter(|&e| e)
+        .count();
 
-        left ^ right
-    }).filter(|&e| e).count();
-    
     println!("part 1: {}", part1_count);
     println!("part 2: {}", part2_count);
 }
