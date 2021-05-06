d52b836deb7bba0e761130afe7b9ca82d77275a5
septa97 <jancharles.adona@gmail.com>
2021-05-06 22:50:44 +0800

    Refactor day 8 to use struct

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,43 +1,51 @@
 use std::fs;
 
+#[derive(Clone)]
+struct Ins {
+    opcode: String,
+    arg: i32,
+}
+
 fn main() {
     let file_string = fs::read_to_string("input/input.txt").expect("Failed to read file!");
-    let instructions: Vec<&str> = file_string
+    let ins_vec: Vec<Ins> = file_string
         .lines()
+        .map(|line| {
+            let splits: Vec<&str> = line.split(' ').collect();
+            let opcode = splits[0].to_string();
+            let arg = splits[1].parse().expect("Failed to parse number to i32!");
+
+            Ins {opcode, arg}
+        })
         .collect();
 
-    println!("part 1: {}", get_acc(&instructions)); // we're sure that there's an infinite loop on the first part
+    println!("part 1: {}", get_acc(&ins_vec)); // we're sure that there's an infinite loop on the first part
 
-    if let Some(acc) = get_acc_fixed_opt(&instructions) {
+    if let Some(acc) = get_acc_fixed_opt(&ins_vec) {
         println!("part 2: {}", acc);
     }
 }
 
-fn get_acc_fixed_opt(instructions: &Vec<&str>) -> Option<i32> {
-    for (idx, &ins) in instructions.iter().enumerate() {
-        let main_split: Vec<&str> = ins.split(' ').collect();
-        let opcode = main_split[0];
-        
-        if opcode == "jmp" {
+fn get_acc_fixed_opt(ins_vec: &Vec<Ins>) -> Option<i32> {
+    for (idx, ins) in ins_vec.iter().enumerate() {
+        if ins.opcode == "jmp" {
             // change to `nop`
-            let mut new_ins = "nop".to_owned();
-            new_ins.push_str(" ");
-            new_ins.push_str(main_split[1]);
-
-            let mut new_instructions = instructions.clone(); // not sure if this is valid?
-            new_instructions[idx] = new_ins.as_str();
+            let mut new_instructions = ins_vec.clone();
+            new_instructions[idx] = Ins {
+                opcode: String::from("nop"),
+                arg: ins.arg
+            };
 
             if !is_infinite_loop(&new_instructions) {
                 return Some(get_acc(&new_instructions));
             }
-        } else if opcode == "nop" {
+        } else if ins.opcode == "nop" {
             // change to `jmp`
-            let mut new_ins = "jmp".to_owned();
-            new_ins.push_str(" ");
-            new_ins.push_str(main_split[1]);
-
-            let mut new_instructions = instructions.clone(); // not sure if this is valid?
-            new_instructions[idx] = new_ins.as_str();
+            let mut new_instructions = ins_vec.clone();
+            new_instructions[idx] = Ins {
+                opcode: String::from("jmp"),
+                arg: ins.arg
+            };
 
             if !is_infinite_loop(&new_instructions) {
                 return Some(get_acc(&new_instructions));
@@ -48,72 +56,60 @@ fn get_acc_fixed_opt(instructions: &Vec<&str>) -> Option<i32> {
     None
 }
 
-fn get_acc(instructions: &Vec<&str>) -> i32 {
+fn get_acc(ins_vec: &Vec<Ins>) -> i32 {
     let mut acc = 0;
     let mut pc = 0; // program counter
-    let mut curr_ins = instructions[pc];
-    let mut ins_exec_vec: Vec<bool> = vec![false; instructions.len()];
+    let mut curr_ins = &ins_vec[pc];
+    let mut ins_exec_vec: Vec<bool> = vec![false; ins_vec.len()];
 
-    while pc < instructions.len() && !ins_exec_vec[pc] {
-        let main_split: Vec<&str> = curr_ins.split(' ').collect();
-        let opcode = main_split[0];
-        let arg: i32 = main_split[1]
-            .parse()
-            .expect("Failed to parse number to i32!");
-
+    while pc < ins_vec.len() && !ins_exec_vec[pc] {
         ins_exec_vec[pc] = true;
-        if opcode == "acc" {
-            acc += arg;
+        if curr_ins.opcode == "acc" {
+            acc += curr_ins.arg;
             pc += 1;
-        } else if opcode == "jmp" {
-            if arg.is_negative() {
-                pc -= arg.abs() as usize;
+        } else if curr_ins.opcode == "jmp" {
+            if curr_ins.arg.is_negative() {
+                pc -= curr_ins.arg.abs() as usize;
             } else {
-                pc += arg as usize;
+                pc += curr_ins.arg as usize;
             }
         } else {
             // we can safely assume that this is "nop"
             pc += 1;
         }
 
-        if pc < instructions.len() {
-            curr_ins = instructions[pc];
+        if pc < ins_vec.len() {
+            curr_ins = &ins_vec[pc];
         }
     }
 
     acc
 }
 
-fn is_infinite_loop(instructions: &Vec<&str>) -> bool {
+fn is_infinite_loop(ins_vec: &Vec<Ins>) -> bool {
     let mut pc = 0; // program counter
-    let mut curr_ins = instructions[pc];
-    let mut ins_exec_vec: Vec<bool> = vec![false; instructions.len()];
+    let mut curr_ins = &ins_vec[pc];
+    let mut ins_exec_vec: Vec<bool> = vec![false; ins_vec.len()];
 
-    while pc < instructions.len() && !ins_exec_vec[pc] {
-        let main_split: Vec<&str> = curr_ins.split(' ').collect();
-        let opcode = main_split[0];
-        let arg: i32 = main_split[1]
-            .parse()
-            .expect("Failed to parse number to i32!");
-
+    while pc < ins_vec.len() && !ins_exec_vec[pc] {
         ins_exec_vec[pc] = true;
-        if opcode == "acc" {
+        if curr_ins.opcode == "acc" {
             pc += 1;
-        } else if opcode == "jmp" {
-            if arg.is_negative() {
-                pc -= arg.abs() as usize;
+        } else if curr_ins.opcode == "jmp" {
+            if curr_ins.arg.is_negative() {
+                pc -= curr_ins.arg.abs() as usize;
             } else {
-                pc += arg as usize;
+                pc += curr_ins.arg as usize;
             }
         } else {
             // we can safely assume that this is "nop"
             pc += 1;
         }
 
-        if pc < instructions.len() {
-            curr_ins = instructions[pc];
+        if pc < ins_vec.len() {
+            curr_ins = &ins_vec[pc];
         }
     }
 
-    pc < instructions.len()
+    pc < ins_vec.len()
 }
